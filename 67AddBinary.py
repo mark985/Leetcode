8d12d21a0f96413aa5d5cfe903541799016ca85b
Huiyu Ma <huiyuma@amazon.com>
2018-11-04 17:33:58 -0800

    67AddBinary.py

--- 67AddBinary.py
+++ 67AddBinary.py
@@ -5,34 +5,11 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        maxLen = max(len(a), len(b))
-        carry = 0
-        result = ''
-        for i in range(maxLen):
-          charA = '0'
-          charB = '0'
-          if len(a) > i:
-            charA = a[len(a) - i - 1]
-          if len(b) > i:
-            charB = b[len(b) - i - 1]
-
-          if carry == 1:
-            if charA == '1' and charB == '1':
-              result = '1' + result
-            elif charA == '0' and charB == '0':
-              result = '1' + result
-              carry = 0
-            else:
-              result = '0' + result
-              carry = 1
-          else:
-            if charA == '1' and charB == '1':
-              result = '0' + result
-              carry = 1
-            elif charA == '0' and charB == '0':
-              result = '0' + result
-            else:
-              result = '1' + result
-        if carry == 1:
-          result = '1' + result
-        return result
+        if len(a) == 0: return b
+        if len(b) == 0: return a
+        if a[-1] == '0' and b[-1] == '0':
+          return self.addBinary(a[0:-1], b[0:-1]) + '0'
+        if a[-1] == '1' and b[-1] == '1':
+          return self.addBinary(self.addBinary(a[0:-1], b[0:-1]), '1') + '0'
+        else:
+          return self.addBinary(self.addBinary(a[0:-1], b[0:-1])) + '1' 
